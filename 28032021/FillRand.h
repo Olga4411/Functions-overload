2f70b4043abcfb199b6c1d3fa8b39a6469ab27a1
Новичковская Ольга <miss4411@yandex.ru>
2021-04-04 22:56:00 +0700

    Functions overload DONE

---
+++ 28032021/FillRand.h
@@ -0,0 +1,5 @@
+#pragma once
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(char arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(float arr[], const int n, int minRand = 0, int maxRand = 100);
